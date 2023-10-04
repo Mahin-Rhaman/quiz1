ffed5f875f8b0b5d6ce0f44f4981e11fa71ff9da
Mahin-Rhaman <97022832+Mahin-Rhaman@users.noreply.github.com>
2023-10-05 00:53:50 +0600

    add new activity

--- app/src/main/java/com/example/quizling/DashboardActivity.kt
+++ app/src/main/java/com/example/quizling/DashboardActivity.kt
@@ -1,11 +1,18 @@
 package com.example.quizling
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        var art=findViewById<Button>(R.id.next_id)
+        art.setOnClickListener {
+            val intent = Intent(this,NextByArtActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/quizling/NextByArtActivity.kt
@@ -0,0 +1,12 @@
+package com.example.quizling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NextByArtActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_next_by_art)
+
+    }
+}
