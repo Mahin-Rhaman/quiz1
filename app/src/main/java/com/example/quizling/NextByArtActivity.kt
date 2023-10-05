4aa190e80c15980aa2733eafb462882d057d9020
Mahin-Rhaman <97022832+Mahin-Rhaman@users.noreply.github.com>
2023-10-05 14:06:44 +0600

    Add new Activity

--- app/src/main/java/com/example/quizling/NextByArtActivity.kt
+++ app/src/main/java/com/example/quizling/NextByArtActivity.kt
@@ -1,71 +1,72 @@
 package com.example.quizling
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.example.quizling.databinding.ActivityNextByArtBinding
 import com.mehedi.simplequiz2302.QuizViewModel
 
 class NextByArtActivity : AppCompatActivity() {
 
-private val viewModel:QuizViewModel by viewModels()
+    private lateinit var viewModel: QuizViewModel
     private lateinit var binding: ActivityNextByArtBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding= ActivityNextByArtBinding.inflate(layoutInflater)
+        binding = ActivityNextByArtBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.nextQuestion.setOnClickListener{
-            val selectOptionIndex=binding.optionsRadioGroup.indexOfChild(findViewById(binding.optionsRadioGroup.checkedRadioButtonId))
+        viewModel = ViewModelProvider(this).get(QuizViewModel::class.java)
 
+        binding.nextQuestion.setOnClickListener {
+            val selectOptionIndex =
+                binding.optionsRadioGroup.indexOfChild(findViewById(binding.optionsRadioGroup.checkedRadioButtonId))
 
-            if (selectOptionIndex ! = -1 ){
+            if (selectOptionIndex != -1) {
                 viewModel.checkAnswer(selectOptionIndex)
-            showNextQuestion()
+                showNextQuestion()
+            } else {
+                Toast.makeText(this, "Select an option", Toast.LENGTH_SHORT).show()
+            }
         }
-            else{
-            android.widget.Toast.makeText(this, "select an Options", Toast.LENGTH_SHORT).show()
-        }
-        }
+
         showCurrentQuestion()
-
-
     }
-    private fun showNExtQuestion() {
+
+    private fun showNextQuestion() {
         val nextQuestion = viewModel.getNextQuestion()
         if (nextQuestion != null) {
             showCurrentQuestion()
-
         } else {
             showResult("Your Score : ${viewModel.getScore()} out of ${viewModel.getQuestionSize()} ")
-
         }
-
-
     }
-    private fun showResult(msg: String) {
 
+    private fun showResult(msg: String) {
         val alertDialog = AlertDialog.Builder(this)
             .setTitle("Your Result!")
             .setMessage(msg)
             .setPositiveButton("Restart") { _, _ ->
                 viewModel.restart()
                 showCurrentQuestion()
-
-            }.setNegativeButton("Dismiss") { _, _ ->
+            }
+            .setNegativeButton("Dismiss") { _, _ ->
                 binding.nextQuestion.visibility = View.INVISIBLE
-
-
-            }.setCancelable(false).create()
+            }
+            .setCancelable(false)
+            .create()
         alertDialog.show()
-
-
     }
-    private fun showCurrentQuestion(){
+
+    private fun showCurrentQuestion() {
         val currentQuestion = viewModel.getCurrentQuestion()
         binding.apply {
-            questionTextView.text=currentQuestion.question
-            option1RadioBtn.text=currentQuestion.options[0]
+            questionTextView.text = currentQuestion.question
+            option1RadioBtn.text = currentQuestion.options[0]
             option2RadioBtn.text = currentQuestion.options[1]
             option3RadioBtn.text = currentQuestion.options[2]
             option4RadioBtn.text = currentQuestion.options[3]
